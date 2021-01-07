1b6e5387fd8a42ac8a667dcb4eba0a95081c6623
Дмитрий Кононенко <dkononenko@MacBook-Air-Dmitrij.local>
2021-01-07 13:38:25 +0300

    Fix limiter

--- middlewares/limiter.js
+++ middlewares/limiter.js
@@ -3,7 +3,7 @@ const { tooManyRequests } = require('../utils/constants');
 
 const limiter = rateLimit({
   windowMs: 1 * 60 * 1000,
-  max: 2000,
+  max: 10000,
   message: {
     message: tooManyRequests,
   },
